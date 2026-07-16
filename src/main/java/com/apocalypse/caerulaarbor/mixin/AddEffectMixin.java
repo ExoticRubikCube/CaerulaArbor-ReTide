71624a8fd288893b25c819e3e76706dbe91493d7
MySQLdisappoint <mysqldisappoint@gmali.com>
2026-07-16 22:34:09 +0800

    fix mixin name

--- src/main/java/com/apocalypse/caerulaarbor/mixin/AddEffectMixin.java
+++ src/main/java/com/apocalypse/caerulaarbor/mixin/AddEffectMixin.java
@@ -16,7 +16,7 @@ import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(value = {LivingEntity.class}, priority = 65536)
-public abstract class AddEffectMixinMixin {
+public abstract class AddEffectMixin {
     @ModifyVariable(
             method = "addEffect(Lnet/minecraft/world/effect/MobEffectInstance;Lnet/minecraft/world/entity/Entity;)Z",
             at = @At("HEAD"),
